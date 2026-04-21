a680707354b7eb2113d5e51471a437664f84a48d
kd1372033 <kd1372033@st.kobedenshi.ac.jp>
2026-04-21 14:02:05 +0900

    02シューティング初回
    01のLogを消去

--- 02シューティング プレイヤー追加/Src/Application/Scene/GameScene.cpp
+++ 02シューティング プレイヤー追加/Src/Application/Scene/GameScene.cpp
@@ -2,25 +2,25 @@
 
 void GameScene::PreUpdate()
 {
-	KdDebugGUI::Instance().AddLog("PreUpdate()\n");
+
 }
 
 void GameScene::Update()
 {
-	KdDebugGUI::Instance().AddLog("Update()\n");
+
 }
 
 void GameScene::Draw()
 {
-	KdDebugGUI::Instance().AddLog("Draw()\n");
+
 }
 
 void GameScene::Init()
 {
-	KdDebugGUI::Instance().AddLog("Init()\n");
+
 }
 
 void GameScene::Release()
 {
-	KdDebugGUI::Instance().AddLog("Release()\n");
+
 }
