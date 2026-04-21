7b19a4e79edd6079a990f9ac27476d4e8db87cc1
kd1372033 <kd1372033@st.kobedenshi.ac.jp>
2026-04-21 13:59:12 +0900

    シーン追加完了
    シーン追加完了

---
+++ 02シューティング プレイヤー追加/Src/Application/Scene/GameScene.cpp
@@ -0,0 +1,26 @@
+﻿#include "GameScene.h"
+
+void GameScene::PreUpdate()
+{
+	KdDebugGUI::Instance().AddLog("PreUpdate()\n");
+}
+
+void GameScene::Update()
+{
+	KdDebugGUI::Instance().AddLog("Update()\n");
+}
+
+void GameScene::Draw()
+{
+	KdDebugGUI::Instance().AddLog("Draw()\n");
+}
+
+void GameScene::Init()
+{
+	KdDebugGUI::Instance().AddLog("Init()\n");
+}
+
+void GameScene::Release()
+{
+	KdDebugGUI::Instance().AddLog("Release()\n");
+}
