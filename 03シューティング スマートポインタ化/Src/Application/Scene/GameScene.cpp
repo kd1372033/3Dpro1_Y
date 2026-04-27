cd1b3debc95442ebb87ae7d0f171e4eaf96c05e0
kd1372033 <kd1372033@st.kobedenshi.ac.jp>
2026-04-27 14:04:13 +0900

    スマートポインタ化
    次回の授業準備

---
+++ 03シューティング スマートポインタ化/Src/Application/Scene/GameScene.cpp
@@ -0,0 +1,34 @@
+﻿#include "GameScene.h"
+#include "../Object/Player/Player.h"	
+
+void GameScene::PreUpdate()
+{
+
+}
+
+void GameScene::Update()
+{
+	m_player->Update();
+}
+
+void GameScene::Draw()
+{
+	m_player->Draw();
+}
+
+void GameScene::Init()
+{
+	// プレイヤー
+	m_player = new Player();	// ①インスタンス確保 基底クラス→派生クラスの順で呼び出される
+	m_player->Init();			// ②初期化
+}
+
+void GameScene::Release()
+{
+	//nullチェック
+	if(m_player)
+	{
+		delete m_player;
+		m_player = nullptr;
+	}
+}
