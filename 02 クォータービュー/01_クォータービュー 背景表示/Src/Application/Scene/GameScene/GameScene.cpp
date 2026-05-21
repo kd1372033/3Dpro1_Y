98eb01aedac6e47454b83772640bc9c58572df10
kd1372033 <kd1372033@st.kobedenshi.ac.jp>
2026-05-21 11:11:45 +0900

    名前変更

---
+++ 02 クォータービュー/01_クォータービュー 背景表示/Src/Application/Scene/GameScene/GameScene.cpp
@@ -0,0 +1,21 @@
+﻿#include "GameScene.h"
+#include"../SceneManager.h"
+
+void GameScene::Event()
+{
+	
+}
+
+void GameScene::Init()
+{
+	// カメラ　生成 & 視野角
+	m_camera = std::make_unique<KdCamera>();	// 1 メモリ確保
+	m_camera->SetProjectionMatrix(60.0f);		// 2 視野角設定
+}
+
+/*
+	ユニークポインタ
+	ユニーク･･･唯一無二
+	アクセスが一つしかないポインタ
+	譲渡のみでしかアクセスできない
+*/
